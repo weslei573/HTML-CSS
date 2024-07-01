f5b4049ef3ad3062a110f48cd2a82e56a7bf9421
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2024-07-01 19:15:37 -0400

    atualizei html

--- Exercícios/ex026/mq005/js/index.js
+++ Exercícios/ex026/mq005/js/index.js
@@ -1,15 +1,15 @@
 function mudouTamanho() {
     if( window.innerWidth >= 768 ){
-        itens.style.display = 'block';
+        itens.style.display = 'block'
     } else {
-        itens.style.display = 'none';
+        itens.style.display = 'none'
     }
-};
+}
 
 function clickMenu() {
     if (itens.style.display == 'block') {
-        itens.style.display = 'none';
+        itens.style.display = 'none'
     } else {
-        itens.style.display = 'block';
+        itens.style.display = 'block'
     }
-};
+}
