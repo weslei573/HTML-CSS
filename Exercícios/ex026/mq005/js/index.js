32eb88272eb3eaf46e773d4a2893da7da683abab
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2024-07-01 17:55:51 -0400

    atualização

--- Exercícios/ex026/mq005/js/index.js
+++ Exercícios/ex026/mq005/js/index.js
@@ -1,15 +1,15 @@
-function clickMenu() {
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
+function mudouTamanho() {
+    if( window.innerWidth >= 768 ){
+        itens.style.display = 'block';
     } else {
-        itens.style.display = 'block'
+        itens.style.display = 'none';
     }
 };
 
-function mudouTamanho() {
-    if( window.innerWidth >= 768 ){
-        itens.style.display = 'block'
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none';
     } else {
-        itens.style.display = 'none'
+        itens.style.display = 'block';
     }
 };
