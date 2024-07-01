6da24d8506d9fda0fce1219f7fa0fa5a79cb7909
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2024-07-01 17:47:59 -0400

    atualizei media queries

--- Exercícios/ex026/mq005/js/index.js
+++ Exercícios/ex026/mq005/js/index.js
@@ -1,7 +1,15 @@
-function clickMenu(){
-    if(itens.style.display == 'block'){
+function clickMenu() {
+    if (itens.style.display == 'block') {
         itens.style.display = 'none'
     } else {
         itens.style.display = 'block'
+    }
+};
+
+function mudouTamanho() {
+    if( window.innerWidth >= 768 ){
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
     }
 };
